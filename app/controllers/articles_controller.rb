08ced666b000a62379ad69add3ba4d8390f12e97
hwoj <hwojnicki41@gmail.com>
2018-07-12 13:58:38 -0400

    show action created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,13 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to article_path(@article)
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+		
 	end
 
 	def show
